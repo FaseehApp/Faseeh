b7babcf025cd9748b4dd63e3e4b7194482164707
Souhail Bektachi <souhalshiv@gmail.com>
2024-11-24 21:52:56 +0100

    refactor useTranscription hook to load transcription data from a local JSON file

--- src/renderer/src/hooks/useTranscription.tsx
+++ src/renderer/src/hooks/useTranscription.tsx
@@ -1,24 +1,16 @@
-import { useEffect, useState, RefObject, useMemo } from "react";
-
+import { useEffect, useState, useMemo } from "react";
+import transcription from './test.json';
 interface TranscriptionSegment {
   startTime: number;
   endTime: number;
   text: string;
 }
 
-function useTranscription(jsonFilePath: string, currentTime: number): string {
+function useTranscription( currentTime: number): string {
     const [transcriptionData, setTranscriptionData] = useState<TranscriptionSegment[]>([]);
-
     useEffect(() => {
-        import(jsonFilePath)
-            .then((data) => {
-                setTranscriptionData(data.default);
-            })
-            .catch((error) => {
-                console.error('Failed to load transcription data:', error);})
-    }, [jsonFilePath]);
-    
-    console.log(transcriptionData);
+        setTranscriptionData(transcription);
+      }, []);    
     const currentTranscription = useMemo(() => {
         return transcriptionData.find(
             t => currentTime >= t.startTime && currentTime <= t.endTime
