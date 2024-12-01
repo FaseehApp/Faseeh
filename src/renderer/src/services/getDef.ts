2879e67cd8a1104d6c1e9484991323a7b9b0e301
Souhail Bektachi <souhalshiv@gmail.com>
2024-12-01 14:38:25 +0100

    fix: return definition from getDef function instead of logging it

--- src/renderer/src/services/getDef.ts
+++ src/renderer/src/services/getDef.ts
@@ -5,7 +5,7 @@ import { ipcRenderer } from "electron";
 
 const getDef = async (def: string) => {
     let definition = await window.electron.ipcRenderer.invoke("get-definition", def);
-    console.log(def,definition);
+    return definition;
 };
 
 
